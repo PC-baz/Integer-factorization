43bd49ecd29a9983f53ee895f0d956f4edaa7bd8
PC-baz <96615624+PC-baz@users.noreply.github.com>
2024-01-06 19:05:09 +0330

    v1.4.0

--- main.py
+++ main.py
@@ -1,82 +1,29 @@
-from lib_s import *
+import argparse
+import math as mt
+
+parser = argparse.ArgumentParser(description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+def check_prime(number):
+   if number < 2:
+       return False
+   for i in range(2, number):
+       if number % i == 0:
+           return False
+   return True
 
 def main(num):
-    # Check if number is Odd or Even by using lib_s.
-    # lib_s.py is a library file that contains a function to check if a number is odd or even.
-    # The function is called check_even_odd() and takes a single integer parameter.
-    # The function returns a string "odd" or "even".
-    # The function is imported at the top of the script using "from lib_s import *".
-    Parsing_bar = "1"
-    if num == 1:
-        print(Parsing_bar)
-        return Parsing_bar
-    if check_prime(num):
-        print(Parsing_bar + "*" + str(num))
-        return Parsing_bar + "*" + str(num)
-    if check_even_odd(num) == "Odd":
-        pr = num
-        while check_prime(num) == False:
-            if check_divisible_by_3(num):
-                Parsing_bar += "*3"
-                num = num // 3
-            if check_divisible_by_5(num):
-                Parsing_bar += "*5"
-                num = num // 5
-            if check_divisible_by_7(num):
-                Parsing_bar += "*7"
-                num = num // 7
-            if check_divisible_by_11(num):
-                Parsing_bar += "*11"
-                num = num // 11
-            if check_divisible_by_13(num):
-                Parsing_bar += "*13"
-                num = num // 13
-            if check_prime(num):
-                Parsing_bar += "*" + str(num)
-                print(Parsing_bar)
-                return Parsing_bar
-            elif num == 1:
-                pass
-            else:
-                Parsing_bar += "*" + str(check_perfect(num))
-                num = num // check_perfect(num)
-            if check_prime(num):
-                Parsing_bar += "*" + str(num)
-            if eval(Parsing_bar) == pr:
-                print(Parsing_bar)
-                return Parsing_bar
-    else:
-        pr = num
-        while check_prime(num) == False:
-            if check_even_odd(num) == "Even":
-                Parsing_bar += "*2"
-                num = num // 2
-            if check_divisible_by_3(num):
-                Parsing_bar += "*3"
-                num = num // 3
-            if check_divisible_by_5(num):
-                Parsing_bar += "*5"
-                num = num // 5
-            if check_divisible_by_7(num):
-                Parsing_bar += "*7"
-                num = num // 7
-            if check_divisible_by_11(num):
-                Parsing_bar += "*11"
-                num = num // 11
-            if check_divisible_by_13(num):
-                Parsing_bar += "*13"
-                num = num // 13
-            if check_prime(num):
-                Parsing_bar += "*" + str(num)
-                print(Parsing_bar)
-                return Parsing_bar
-            elif num == 1:
-                pass
-            else:
-                Parsing_bar += "*" + str(check_perfect(num))
-                num = num // check_perfect(num)
-            if check_prime(num):
-                Parsing_bar += "*" + str(num)
-            if eval(Parsing_bar) == pr:
-                print(Parsing_bar) 
-                return Parsing_bar
+        while num % 2 == 0:
+            yield 2
+            num = num // 2
+        for i in range(3,int(mt.sqrt(num))+1,2):
+            if num <= i:
+                break
+            while num % i == 0:
+                yield i
+                num = num // i
+        if num > 2:
+            yield num
+def generator2str(number):
+    return '*'.join(map(lambda x: str(x), main(number)))
+if __name__ == '__main__':
+    pass
+
